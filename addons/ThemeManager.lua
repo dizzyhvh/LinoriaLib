6b04eb7bf5e7fbc13595565634d7143b31315b35
dizzy hub <126523396+dizzyhvh@users.noreply.github.com>
2025-04-04 13:30:21 +0200

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -5,7 +5,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Forest Fire'] 		= { 1, httpService:JSONDecode('{"MainColor":"d4ff00","AccentColor":"ff0000","OutlineColor":"ff9900","BackgroundColor":"ff0000","FontColor":"ffffff"}') },
+		['Forest Fire'] 		= { 1, httpService:JSONDecode('{"MainColor":"550000","AccentColor":"ffb800","OutlineColor":"000000","BackgroundColor":"d10000","FontColor":"ffffff"}') },
 		['MS Paint'] 		= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1c1c1c","AccentColor":"0055ff","BackgroundColor":"141414","OutlineColor":"323232"}') },
 		['BBot'] 			= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
 		['Fatality']		= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
